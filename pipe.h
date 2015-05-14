f89731d6424cd1526511c4a370c596439dce1b78
Murph9 <Murph9y@gmail.com>
2015-05-14 16:12:34 +1000

    added the initial files

---
+++ pipe.h
@@ -0,0 +1,8 @@
+/*********************************************
+ *  pipe.h
+ *  Socket Interface for Text-Based Adventure Game
+ *  COMP3411 Artificial Intelligence
+ *  UNSW Session 1, 2012
+*/
+
+int tcpopen(char *host, int port);
